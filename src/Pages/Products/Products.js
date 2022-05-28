646beed53208cc7e3fb4ceb51fca224f2cb4a5e9
mahbubur1 <mdmahbuborr462@gail.com>
2022-05-28 22:27:17 +0600

    file update

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -21,7 +21,7 @@ const Products = () => {
             <Helmet>
                 <title>Products - KING BD</title>
             </Helmet>
-            <h3 className='text-2xl text-center font-bold my-8'><span className=' border-b-2 border-primary'>All Your Needs</span></h3>
+            <h3 className='text-2xl text-center font-bold my-8'><span className=' border-b-2 border-primary'>All You Needs</span></h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10 pl-6'>
                 {
                     products?.map(product => <Product key={product._id} product={product} />)
