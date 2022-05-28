9211c5db9cba4f6fa65cdb0e7aafdb39e44eaa58
mahbubur1 <mdmahbuborr462@gail.com>
2022-05-28 12:57:33 +0600

    primary color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#E03B8B",
+          primary: "#116f9e",
           secondary: "#f6d860",
           accent: "#37cdbe",
           neutral: "#3d4451",
