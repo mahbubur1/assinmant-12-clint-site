36e513fbad7ab66247e7ed11dac0c9307a9ccc66
mahbubur1 <mdmahbuborr462@gail.com>
2022-05-28 22:52:35 +0600

    final

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import me from '../../Assests/me.jpg';
 
 const Portfolio = () => {
     return (
         <>
-            <h3 className='text-2xl text-center font-bold my-8'><span className=' border-b-2 border-primary'>Developer Portfolio</span></h3>
+            <h3 className='text-2xl text-center font-bold my-8'><span className=' border-b-2 border-primary'>DEVELOPER PORTFOLIO</span></h3>
 
             <div>
                 <div class="avatar flex justify-center">
@@ -14,11 +14,11 @@ const Portfolio = () => {
                     </div>
                 </div>
                 <div className=''>
-                    <h3 className='text-2xl text-center text-indigo-900 bg-clip-text bg-gradient-to-br from-accent to-secondary mt-3 font-bold'>Mahbubur Rahman</h3>
+                    <h3 className='text-3xl text-center text-indigo-900 bg-clip-text bg-gradient-to-br from-accent to-secondary mt-3 font-bold'>Mahbubur Rahman</h3>
                     <div className='flex flex-col justify-center items-center mb-6'>
                         <h3 className='text-lg text-sky-600'>mdmahbuborr462@gmail.com</h3>
-                        <a href='https://www.linkedin.com/in/md-mahbubor-rahman-a2870723b/' class="text-lg btn-active btn-info btn-link">Linkedin profile</a >
-                        <a href='https://mahbubur1.github.io/assinmant-1/' class="text-lg btn-active btn-info btn-link">Portfolio</ a>
+                        <a target='-blank' href='https://www.linkedin.com/in/md-mahbubor-rahman-a2870723b/' class="text-lg btn-active btn-info btn-link">Linkedin profile</a >
+                        <a target='-blank' href='https://mahbubur1.github.io/assinmant-1/' class="text-lg btn-active btn-info btn-link">Portfolio</ a>
                     </div>
                     <div className='flex justify-center items-center mb-10'>
                         <div class="card w-full md:w-2/4 lg:w-2/5 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-primary-content">
@@ -53,25 +53,22 @@ const Portfolio = () => {
                         </div>
                     </div>
                     <div className='flex justify-center items-center mb-10'>
-                        <div class="card w-full md:w-2/4 lg:w-2/5 bg-gradient-to-r from-purple-500 to-pink-500 text-primary-content">
+                        <div class="card w-full md:w-2/4 lg:w-2/5 bg-gradient-to-r from-purple-400 to-blue-600 text-primary-content">
                             <div class="card-body">
                                 <h2 class="card-title">Projects</h2>
                                 <div className='grid grid-cols-1'>
-                                    <p> <span className='text-md font-bold'>1. Auto Showroom</span> <br />
-                                        <Link className='btn-link btn-active' to='https://mads-auto-showroom.web.app/' >Live Site Link</Link> <br />
-                                        <Link className='btn-link btn-active' to='https://github.com/MrMADS28289/Auto-Showroom' >Client Side GitHub Link</Link> <br />
-                                        <Link className='btn-link btn-active' to='https://github.com/MrMADS28289/Auto-Showroom-Server' >Server Side GitHub Link</Link> <br />
-
-                                        It’s a car's dealer website.
-                                        For buy and sell cars.
+                                    <p> <span className='text-md font-bold'>1. Mahbub Photography</span> <br />
+                                        <a target='-blank' className='btn-link btn-active' href='https://independent-service-prov-72389.web.app/' >Live Site Link</a> <br />
+                                        <a target='-blank' className='btn-link btn-active' href='https://github.com/programming-hero-web-course-4/independent-service-provider-mahbubur1' > GitHub Link</a> <br />
+                                        It's a Phothography service provide website
                                         User can log in with email and Facebook.
                                     </p>
-                                    <p> <span className='text-md font-bold'>2. My To-Do app</span> <br />
-                                        <Link className='btn-link btn-active' to='https://mads-to-do-list.web.app/login' >Live Site Link</Link> <br />
-                                        <Link className='btn-link btn-active' to='https://github.com/MrMADS28289/To-Do-List' >Client Side GitHub Link</Link> <br />
-                                        <Link className='btn-link btn-active' to='https://github.com/MrMADS28289/To-Do-List-server' >Server Side GitHub Link</Link> <br />
+                                    <p> <span className='text-md font-bold'>2. Halal Food</span> <br />
+                                        <a target='-blank' className='btn-link btn-active' href='https://halal-food-corner.netlify.app/' >Live Site Link</a> <br />
+                                        <a target='-blank' className='btn-link btn-active' href='https://github.com/ProgrammingHeroWC4/warehouse-management-client-side-mahbubur1'>Client Side GitHub Link</a> <br />
+                                        <a target='-blank' className='btn-link btn-active'href='https://github.com/ProgrammingHeroWC4/warehouse-management-server-side-mahbubur1' >Server Side GitHub Link</a> <br />
 
-                                        It's a To Do check list website. You can note you'r daily task, and that's secure. You need to login first of all.
+                                        It's a Food seller website. You can order your food, and your manage your order.
                                     </p>
 
                                 </div>
